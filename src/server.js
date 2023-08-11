4669e492a1940e8c884aa5bd81c24ca01161c70a
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2023-08-11 20:18:49 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,16 +9,16 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(route);
-app.get("*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
 app.all('*', (req, res) => {
     res.status(404).send('Ooops page not found');
 });
