9d958a3ebcaad92270d0ffb48da86fc146f24f6e
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2023-08-11 20:03:03 +0530

    ecommerce app

--- src/server.js
+++ src/server.js
@@ -9,10 +9,15 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(route);
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+app.get("*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
 app.all('*', (req, res) => {
     res.status(404).send('Ooops page not found');
